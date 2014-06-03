115844b6c515fc73a64657e00a6624c7834b49aa
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-06-03 17:43:59 +0200

    fix bugs

--- app/controllers/client/liste_acheters_controller.rb
+++ app/controllers/client/liste_acheters_controller.rb
@@ -1,35 +1,35 @@
 class Client::ListeAchetersController < ApplicationController
-	def index
-		@client = client
-		@listes= ListeAcheter.where(client_id: @client.id)
-	end
+  def index
+    @client = client
+    @listes= ListeAcheter.where(client_id: @client.id)
+  end
 
   def new
     @client = client
-    @achat = @client.liste_acheter.new
-    @achat.save
+    @favorie = @client.liste_acheter.new
+    @favorie.save
   end
  
   def create
     @client = client
     @produit = produit
-    @produit_existe = ListeAcheter.exists?(produit_id: @produit.id)
-
-    if @produit_existe
+    @produit_exite = ListeAcheter.exists?(produit_id: @produit.id)
+    
+    if @produit_exite
       @message= "Produit existed"
     else
-      @acheter = @client.liste_acheter.create(client_id: @client.id, produit_id: @produit.id)
-      @acheter.save
-      if @acheter.save
-         flash[:notice] =  "produit has been successfully added in liste à acheter."
+      @achat = @client.liste_acheter.create(client_id: @client.id, produit_id: @produit.id)
+    @achat.save
+      if @achat.save
+         flash[:notice] =  "produit has been successfully added in liste favoris."
       else 
         flash[:notice] =  "erroooooo"
     end
   end
-end
-  
-def destroy
-   @client = client
+  end
+
+ def destroy
+    @client = client
     @produit = @client.liste_acheter.find(params[:id])
     @produit.destroy
 
@@ -42,21 +42,21 @@ def destroy
 
 private
   def liste_acheter_params
-    params.require(:liste_acheter).permit(:client_id, :produit_id) if params[:liste_acheter]
+      params.require(:liste_acheter).permit(:client_id, :produit_id) if params[:liste_acheter]
+  
   end
 
   def client
-  	if current_client.id
+    if current_client.id
       id = current_client.id
       Client.find(current_client.id)
     end
   end
 
   def produit
-  	if params[:produit_id]
+    if params[:produit_id]
       id = params[:produit_id]
       Produit.find(params[:produit_id])
-  	end
+    end
   end
-
 end
