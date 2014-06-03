9b4c6e0e329e604d86bb4a3aad7e6c96e4392c18
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-06-03 15:34:42 +0200

    nb p

--- db/migrate/20140505135838_add_refs_to_produits.rb
+++ db/migrate/20140505135838_add_refs_to_produits.rb
@@ -3,7 +3,6 @@ class AddRefsToProduits < ActiveRecord::Migration
     add_reference :produits, :source, index: true
     add_reference :produits, :type_de_produit, index: true
 
-    add_column :produits, :nombre, :integer
   	add_column :produits, :nfc_id, :string
   end
 end
